c84069e0fde4dae348e4d33ad342b45b2fb86055
Danylo Morhun <danymorhun@gmail.com>
2026-07-07 23:45:16 +0200

    feat: add nuxt config

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/sitemap', '@nuxtjs/robots'],
+
+  css: ['~/assets/css/main.css'],
+
+  site: {
+    url: 'https://www.danylomorhun.com',
+    name: 'Danylo Morhun',
+  },
+
+  sitemap: {
+    zeroRuntime: true,
+  },
+
+  app: {
+    head: {
+      htmlAttrs: { lang: 'en' },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
+    },
+  },
+})
