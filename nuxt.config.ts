89a8fe980f086aa366e0f4c4c205f88097c62bf1
Danylo Morhun <danymorhun@gmail.com>
2026-07-08 22:28:32 +0200

    feat(favicon): add adaptive svg favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,7 @@ export default defineNuxtConfig({
     head: {
       htmlAttrs: { lang: 'en' },
       link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
